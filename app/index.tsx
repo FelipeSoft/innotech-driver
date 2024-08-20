2b405147d4ca85879b19f5d4c59467faa55c50d4
FelipeSoft <felipedecastrolima2@gmail.com>
2024-08-20 14:21:46 -0300

    [feat] Adicionando funcionalidade de mostrar dia atual.

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import { MainLayout } from "./layouts/main-layout";
 
 const Index = () => {
     return (
-        <MainLayout>
-            <View className="w-full flex items-center justify-center dark:bg-slate-950 bg-slate-100">
+        <MainLayout sectionTitle="Home">
+            <View className="w-full flex items-center justify-center">
                 <Link href="/session-start" asChild>
                     <Pressable className="w-full py-2 px-4 text-xl rounded-md bg-white dark:bg-slate-900">
                         <View className="w-full flex items-center justify-between">
--- app/layouts/main-layout.tsx
+++ app/layouts/main-layout.tsx
@@ -1,30 +1,54 @@
-import { View, Text, TextInput, TouchableHighlight } from "react-native";
+import { View, Text, TouchableHighlight } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useState, useEffect } from "react";
 
 type Props = {
     children: React.ReactNode;
+    sectionTitle: string;
 }
 
-export const MainLayout = ({ children }: Props) => {
+export const MainLayout = ({ children, sectionTitle }: Props) => {
+    const [today, setToday] = useState("");
+
+    useEffect(() => {
+        const today = new Date();
+        const weekDays = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+        const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+        const weekDay = today.getDay();
+        const monthDay = today.getDate();
+        const year = today.getFullYear();
+        const month = today.getMonth();
+        setToday(`${weekDays[weekDay]}, ${monthDay} de ${months[month]} de ${year}.`);
+    }, []);
+
     return (
         <>
-            <View className="w-full h-full flex flex-col dark:bg-slate-950 bg-slate-100">
-                <View className="pt-12 pb-6 px-6 flex flex-col justify-start bg-blue-600">
-                    <Text className="font-light text-md text-white">Segunda-feira, Agosto de 2024</Text>
-                    <Text className="font-semibold text-2xl text-white">Acompanhamento de Entregas</Text>
+            <View className="w-full h-full flex flex-col dark:bg-slate-950">
+                <View className="pt-16 pb-6 px-6 flex flex-col justify-start bg-blue-600">
+                    <Text className="font-light text-md text-white">{today}</Text>
+                    <Text className="font-semibold text-2xl text-white">{sectionTitle}</Text>
                 </View>
                 <View className="px-6">{children}</View>
-                <View className="px-6 flex flex-row items-center justify-between h-[80px] bg-white dark:bg-slate-950 absolute bottom-0 w-full">
+                <View className="px-10 flex flex-row items-center justify-between h-[80px] bg-blue-600 absolute bottom-0 w-full">
                     <TouchableHighlight>
-                        <Text className="bg-blue-600 h-12 w-12 rounded-md text-white flex items-center justify-center">I</Text>
+                        <View className="flex flex-col items-center justify-center">
+                            <Ionicons name="home-outline" size={26} color="white" />
+                        </View>
                     </TouchableHighlight>
                     <TouchableHighlight>
-                        <Text className="bg-blue-600 h-12 w-12 rounded-md text-white flex items-center justify-center">I</Text>
+                        <View className="flex flex-col items-center justify-center">
+                            <Ionicons name="map-outline" size={26} color="white" />
+                        </View>
                     </TouchableHighlight>
                     <TouchableHighlight>
-                        <Text className="bg-blue-600 h-12 w-12 rounded-md text-white flex items-center justify-center">I</Text>
+                        <View className="flex flex-col items-center justify-center">
+                            <Ionicons name="cube-outline" size={26} color="white" />
+                        </View>
                     </TouchableHighlight>
                     <TouchableHighlight>
-                        <Text className="bg-blue-600 h-12 w-12 rounded-md text-white flex items-center justify-center">I</Text>
+                        <View className="flex flex-col items-center justify-center">
+                            <Ionicons name="settings-outline" size={26} color="white" />
+                        </View>
                     </TouchableHighlight>
                 </View>
             </View>
